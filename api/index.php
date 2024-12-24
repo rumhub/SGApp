58b0f43d3355016b5a716c1d8441c5d6a1a03c73
rumhub <boxyuto@gmail.com>
2024-12-24 13:23:56 +0100

    update index.php

--- api/index.php
+++ api/index.php
@@ -28,7 +28,7 @@
       <tbody>
         <?php
 
-        $conexion = mysqli_connect(getenv('MYSQL_HOST'), getenv('MYSQL_USER'), getenv('MYSQL_PASSWORD'), "SG");
+        $conexion = mysqli_connect('34.72.36.173', 'root', 'xxx', "SG");
 
         $cadenaSQL = "select * from s_customer";
         $resultado = mysqli_query($conexion, $cadenaSQL);
