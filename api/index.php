c82b5eb66e69e900bd6ba409b5ae7b9523b11add
rumhub <boxyuto@gmail.com>
2024-12-24 14:35:33 +0100

    Fix index.php

--- api/index.php
+++ api/index.php
@@ -1,12 +1,31 @@
+GNU nano 4.8                             /var/www/html/SGApp/index.php                                        
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+// Database connection
+$conexion = mysqli_connect('34.163.186.204', 'root', 'xxx', 'SG');
+if (!$conexion) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+// SQL query
+$cadenaSQL = "SELECT * FROM s_customer";
+$resultado = mysqli_query($conexion, $cadenaSQL);
+if (!$resultado) {
+    die("Query failed: " . mysqli_error($conexion));
+}
+?>
+<!DOCTYPE html>
 <html>
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title>Customer Catalog</title>
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css">
 </head>
 <body>
-  <div class = "container">
+  <div class="container">
     <div class="jumbotron">
       <h1 class="display-4">Customer Catalog</h1>
       <p class="lead">Customer Catalog Sample Application</p>
@@ -16,40 +35,38 @@
     <table class="table table-striped table-responsive">
       <thead>
         <tr>
-          <th>Name</th>
-          <th>Credit Rating</th>
-          <th>Address</th>
-          <th>City</th>
-          <th>State</th>
-          <th>Country</th>
-          <th>Zip</th>
-        </tr>
-      </thead>
-      <tbody>
-        <?php
-
-        $conexion = mysqli_connect('34.72.36.173', 'root', 'xxx', "SG");
-
-        $cadenaSQL = "select * from s_customer";
-        $resultado = mysqli_query($conexion, $cadenaSQL);
-
-        while ($fila = mysqli_fetch_object($resultado)) {
-         echo "<tr><td> " .$fila->name . 
-         "</td><td>" . $fila->credit_rating .
-         "</td><td>" . $fila->address .
-         "</td><td>" . $fila->city .
-         "</td><td>" . $fila->state .
-         "</td><td>" . $fila->country .
-         "</td><td>" . $fila->zip_code .
-         "</td></tr>";
-       }
-       ?>
-     </tbody>
-   </table>
- </div>
- <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
- <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
- <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+            <th>Name</th>
+            <th>Credit Rating</th>
+            <th>Address</th>
+            <th>City</th>
+            <th>State</th>
+            <th>Country</th>
+            <th>Zip</th>
+          </tr>
+        </thead>
+        <tbody>
+          <?php
+          if (mysqli_num_rows($resultado) > 0) {
+              while ($fila = mysqli_fetch_object($resultado)) {
+                  echo "<tr>
+                          <td>{$fila->name}</td>
+                          <td>{$fila->credit_rating}</td>
+                          <td>{$fila->address}</td>
+                          <td>{$fila->city}</td>
+                          <td>{$fila->state}</td>
+                          <td>{$fila->country}</td>
+                          <td>{$fila->zip_code}</td>
+                        </tr>";
+              }
+          } else {
+              echo "<tr><td colspan='7'>No data found</td></tr>";
+          }
+          ?>
+      </tbody>
+    </table>
+  </div>
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
 </body>
 </html>
-
